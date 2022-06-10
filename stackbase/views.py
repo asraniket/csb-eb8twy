69b68ca7a1397714c2ddc4ec1cb35ef3e3fa2d4e
Code Sandbox <hello@codesandbox.io>
2022-06-10 09:04:17 +0000

    Deployment from Codesandbox.

---
+++ stackbase/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Question
+# Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+#CRUD Function
+class QuestionListView(ListView):
+	model = Question
+	context_object_name = 'questions'
+	ordering = ['-date_created']
+
+class QuestionDetailView(DetailView):
+	model = Question
+	
+class QuestionCreateView(CreateView):
+	model = Question
+	fields = ['title', 'content']
+
+	def form_valid(self,form):
+		form.instance.user = self.request.user
+		return super().form_valid(form)
